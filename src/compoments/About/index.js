8d406e61cfad527eda17d7dd229de1f9b9d86a14
stefanorn <stefanorn92@gmail.com>
2021-02-19 10:22:09 +0100

    updade about us to include where we get our data

--- src/compoments/About/index.js
+++ src/compoments/About/index.js
@@ -20,7 +20,6 @@ const About = () => (
           Vi får dataene våre fra <a href='https://www.fiskeridir.no'>fiskeridir.no</a> som har en åpen database,
           ikke alle vet det hvordan du får tilgang til denne informasjonen, 
           og det kan være vanskelig å tyde.
-.
           </p>
           <p>
             Vi ønsker å gi enkel tilgang til denne informasjonen
